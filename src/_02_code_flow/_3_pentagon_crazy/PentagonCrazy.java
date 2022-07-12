fcad0bd2c977206aae383124162e1bc094e31393
league <league@172.25.113.55>
2022-07-12 16:31:34 -0700

    First time coding.

--- src/_02_code_flow/_3_pentagon_crazy/PentagonCrazy.java
+++ src/_02_code_flow/_3_pentagon_crazy/PentagonCrazy.java
@@ -11,23 +11,23 @@ public class PentagonCrazy {
 	
 	public static void main(String[] args) {
 		
-		//  Create a new Robot
-
+		//  Create a new Robot	
+		Robot bank = new Robot ();
 		//  Put the robot's pen down
-
+		bank.penDown();
 		//  SPEED. Make the robot go at maximum speed (100)
-
+		bank.setSpeed(100);
 		//  COLOR. Set the pen to a color that you like for the shape
-
+		bank.setPenColor(255,0,0);
 		//  NUMBER OF SIDES. Make an int variable for the number of sides the shape will have.
 		//  				(Hint: its called PentagonCrazy)
-
+		int sides =5;
 		//  TURN ANGLE. Make another int variable for the angle the robot must turn. 
 		//  			Hint: Divide 360 by the number of sides the shape has to get the angle.
-		
+		int angle = 360/sides;
 		//  COUNT. Make another int variable to count how many times the loop has repeated
 		//         Set its start value to zero.
-
+		
 		//  LOOP. Start a while loop to repeat the DRAW, TURN, and INCREASE COUNT code 200 times. 
 		
 				//  DRAW.  Make the robot move the amount in your count variable
