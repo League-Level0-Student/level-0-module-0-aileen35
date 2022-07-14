57fc1bacb385117ed4373c45d290c605012f5e7a
league <league@172.25.113.55>
2022-07-14 16:41:58 -0700

    worked on java

--- src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
+++ src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
@@ -11,9 +11,9 @@ public class MadlibsOfTheAmazon {
 		// Get the player to enter an adjective
 		String adjective= JOptionPane.showInputDialog("Enter an adjective");
 		// Get the player to enter a type of liquid
-		String liquid=JOptionPane.showInputDialog("Enter a liqud");
+		String liquid=JOptionPane.showInputDialog("Enter a liquid");
 		// Get the player to enter a body part
-		String bodypart=JOptionPane.showInputDialog("Enter a bodypart");
+		String bodyPart=JOptionPane.showInputDialog("Enter a body part");
 		// Get the player to enter a verb
 		String verb=JOptionPane.showInputDialog("Enter a verb");
 		// Get the player to enter a place
@@ -24,15 +24,15 @@ public class MadlibsOfTheAmazon {
 		// Hint:  You will need to add more + signs to join the variables to the other parts of the story.
 		
 		String story = 
-		"Piranhas are more [**adjective**] during the day, so cross the river at\n"  +
-		"night. Piranhas are attracted to fresh [**type of liquid**] and will most\n"  +
-		"likely take a bite out of your [**body part**] if you [**verb**]. Whatever\n"  +
+		"Piranhas are more " +adjective+ " during the day, so cross the river at\n"  +
+		"night. Piranhas are attracted to fresh " +liquid+ " and will most\n"  +
+		"likely take a bite out of your " +bodyPart+ " if you " +verb+ " Whatever\n"  +
 		"you do, if you have an open wound, try to find another way to get\n"  +
-		"back to the [**place**]. Good luck!";
+		"back to the " +place+ " Good luck!";
 		
 		// Make a pop-up that contains the final story. The \n escape characters add line breaks to the story. 
 		// If you need to, move them around to make your story look better in the pop-up
-		
+		JOptionPane.showMessageDialog(null,story);
 		// If you want to write your own Madlib story, just change the story variable and ask the player different questions.
 
 	}
--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,11 +8,17 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String code = String input = JOptionPane.showInputDialog("What is your name?");("Do you know how to write code?");
 		// 2. If they say "yes", tell them they will rule the world.
-
+		if (code.equalsIgnoreCase("yes")) {
+			JOptionPane.showMessageDialog(null,  "you will rule the world. ");
+		}
+		else {
+			JOptionPane.showMessageDialog(null,  "sign up for the league classes ");
+		
+		}
 		// 3. Otherwise, tell them to sign up for classes at The League.
-
+		
 	}
 }
 
